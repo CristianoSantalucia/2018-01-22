a1442df68e8e9a83fff096591bf7b4f383f1b6b9
cristiano <cristiano.santalucia>
2021-05-27 14:19:26 +0200

    setup

---
+++ src/it/polito/tdp/seriea/Model.java
@@ -0,0 +1,6 @@
+package it.polito.tdp.seriea;
+
+public class Model
+{
+	
+}
---
+++ src/it/polito/tdp/seriea/TestModel.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.seriea;
+
+public class TestModel
+{
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
